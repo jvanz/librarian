f9cbf8bec80fb3744eefa6a8000ef5a624711d28
José Guilherme Vanz <guilherme.sft@gmail.com>
2018-07-11 00:53:52 -0300

    main.cc: add simple program options
    Adds simple program options to show how we can use it in the future
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -6,7 +6,19 @@
 using namespace seastar;
 using namespace std;
 
+namespace bpo = boost::program_options;
+
 int main(int argc, char** argv)
 {
 	app_template app;
+	app.get_options_description().add_options()
+		("sstringopt", bpo::value<sstring>(), "sstring options")
+		("stdstringopt", bpo::value<string>(), " std string options");
+
+	bpo::variables_map vm;
+	bpo::store(bpo::command_line_parser(argc, argv).options(app.get_options_description()).allow_unregistered().run(), vm);
+
+	cout << "sstringopt: " << vm["sstringopt"].as<sstring>() << endl;
+	cout << "stdstringopt: " << vm["stdstringopt"].as<string>() << endl;
+
 }
